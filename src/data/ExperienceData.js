37573ca564278d3141b86f2fda87ef43b7c9afa0
prajaktanisal99 <prajaktanisal99@gmail.com>
2025-02-06 00:47:47 -0600

    Update Experience

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -1,42 +1,34 @@
 export const ExperienceList = [
   {
-    title: "Graduate Teaching Assistant, ",
+    title: "Graduate Teaching Assistant",
     organization: "The University of Texas, Arlington",
     contributions: [
       "Led programming, digital logic, and embedded systems labs, guiding students in software design and debugging.",
       "Assessed student projects with a focus on algorithms, code structure, and hardware-software integration.",
     ],
-    // techStack:
-    // "Java, JavaScript, Typescript, Spring Boot, React, Jest, RESTful APIs, Google Map Integration",
-    // imgUrl: "/path/to/image1.jpg",
     date: "Aug 2024 - Present",
   },
   {
     title: "Software Engineer I",
     organization: "DWS India Private Limited(Deutsche Bank Group)",
-
-    description:
-      "A cross-platform mobile game (in closed testing) where players try to find the missing pieces of a story.",
     contributions: [
- "Designed and developed a highly interactive, user-centric asset management dashboard using React.js, enhancing user productivity by 40%",
-"Implemented optimization techniques like code-splitting and lazy loading, reducing the initial load time by 50%, improving overall application performance.",
-"Optimized rendering of large datasets by implementing virtualized lists, improving front-end responsiveness for high-trahic operations.",
-"Collaborated with backend teams to implement scalable RESTful APIs, improving data fetching and reducing page load times by 75%.",
-"Utilized Redux for state management to ensure smooth application performance, enhancing the app’s scalability and reducing re-renders.",
+      "Contributed to a React.js asset management dashboard, improving user productivity by 40% and cutting load times by 50% through optimization techniques like code-splitting and lazy loading.",
+      "Overhauled custom React hooks, significantly enhancing code reusability, modularity, & maintainability across project.",
+      "Optimized huge dataset rendering using virtualized lists, AG-Grid thus improving front-end responsiveness and performance during high-traﬃc operations.",
+      "Implemented scalable RESTful APIs, cutting page load times by 75% and boosting data-fetching eﬃciency.",
+      "Utilized Context API for eﬃcient state management, ensuring application scalability while minimizing re-renders.",
+      "Deployed CI/CD pipelines with Jenkins/TeamCity, streamlining automation and speeding up release cycles.",
+      "Troubleshot & enhanced test coverage to 95%+ using Jest & React Testing Library, ensuring code quality and stability.",
     ],
     date: "Jul 2021 - Aug 2023",
-    // document: "https://youknowtheending.net/",
   },
   {
     title: "Software Engineer Intern",
     organization: "Deutsche India Private Limited",
-    description:
-      "A cross-platform mobile game (in closed testing) where players try to find the missing pieces of a story.",
     contributions: [
-      "Build an attribute-level data lineage solution using Hadoop, Spark, and Scala, enhancing data tracking and reducing debugging time by 60%.",
-      "Developed RESTful APIs for ingesting data from external sources and generating data flow visualizations through the Spline framework",
+      "Built attribute-level data lineage solution with Spark & Scala, reducing debugging time by 60% and refining lineage.",
+      "Designed RESTful APIs for data ingestion and lineage tracking with Spline, improving compliance and governance.",
     ],
     date: "Jun 2020 - Jul 2020",
-    // document: "https://youknowtheending.net/",
   },
 ];
