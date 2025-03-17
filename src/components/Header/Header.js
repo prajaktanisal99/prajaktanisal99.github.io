39adb2bd19d3960f8abae36b88d6030830f61315
prajaktanisal99 <prajaktanisal99@gmail.com>
2025-03-17 02:47:04 -0500

    Resume updated.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,7 +31,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://drive.google.com/file/d/1asUOvbTVQVofm_Uw5Q2Nz8vup5Fcyn2m/view?usp=sharing"
+            href="https://drive.google.com/file/d/1wA-BwSjHUzqXv3ssSCk_hxbAUyAXnMIo/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
